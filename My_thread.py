ae91344a0edb44b6be274b383ec236cd35d26453
Paul-JD <jourdindargougepaul@gmail.com>
2023-08-31 22:33:04 +0200

    comit 1 soir 31/08 controle temps exec

---
+++ My_thread.py
@@ -0,0 +1,56 @@
+from collections import Counter
+from threading import Thread
+import pandas as pd
+
+
+class My_thread(Thread):
+    def __init__(self, id, dataset, colonnes):
+        super(My_thread, self).__init__()
+        self.id = id
+        self.dataset = dataset
+        self.colonnes = colonnes
+        self.retour = pd.DataFrame()
+
+    def run(self) -> None:
+        tmp_dataset = self.dataset
+        index = tmp_dataset.index[0]
+        tmp_dict = dict.fromkeys(self.colonnes)
+
+        tmp_dict['date_mutation'] = [tmp_dataset.loc[index, 'date_mutation']]
+        tmp_dict['code_postal'] = [tmp_dataset.loc[index, 'code_postal']]
+        tmp_dict['code_commune'] = [tmp_dataset.loc[index, 'code_commune']]
+        tmp_dict['code_departement'] = [tmp_dataset.loc[index, 'code_departement']]
+        tmp_dict['nombre_lots'] = [tmp_dataset.loc[index, 'nombre_lots'].sum()]
+        tmp_dict['surface_reelle_bati'] = [tmp_dataset.loc[index, 'surface_reelle_bati'].sum()]
+        tmp_dict['nombre_pieces_principales'] = [tmp_dataset.loc[index, 'nombre_pieces_principales'].sum()]
+        tmp_dict['surface_terrain'] = [tmp_dataset.loc[index, 'surface_terrain'].sum()]
+        tmp_dict['valeur_fonciere'] = [tmp_dataset.loc[index, 'valeur_fonciere']]
+        tmp_dict['longitude'] = [tmp_dataset.loc[index, 'longitude']]
+        tmp_dict['latitude'] = [tmp_dataset.loc[index, 'latitude']]
+
+        # valeur classe code type local
+        count_type_local = Counter(tmp_dataset.loc[:, 'type_local'])
+        for type_local in count_type_local:
+            tmp_dict[type_local] = count_type_local[type_local]
+
+        # valeur prefixe voie
+        count_prefixe_voie = Counter(tmp_dataset.loc[:, 'prefixe_voie'])
+        for abbrev_voie in count_prefixe_voie:
+            tmp_dict[abbrev_voie] = count_prefixe_voie[abbrev_voie]
+
+        # valeur classe nature mutation
+        count_nature_mutation = Counter(tmp_dataset.loc[:, 'nature_mutation'])
+        for nat_mutation in count_nature_mutation:
+            tmp_dict[nat_mutation] = count_nature_mutation[nat_mutation]
+
+        # valeur classe culture
+        count_type_culture = Counter(tmp_dataset.loc[:, 'code_nature_culture'])
+        for culture in count_type_culture:
+            tmp_dict[culture] = count_type_culture[culture]
+
+        # valeur classe culture
+        count_type_culture_spe = Counter(tmp_dataset.loc[:, 'code_nature_culture_speciale'])
+        for culture_spe in count_type_culture_spe:
+            tmp_dict[culture_spe] = count_type_culture_spe[culture_spe]
+
+        self.retour = pd.DataFrame.from_dict(tmp_dict)
---
+++ Test.py
@@ -0,0 +1,119 @@
+import gzip
+import io
+from collections import Counter
+from threading import Thread
+import time
+import numpy as np
+import pandas as pd
+import requests
+
+from My_thread import My_thread
+
+url = "https://files.data.gouv.fr/geo-dvf/latest/csv/2018/full.csv.gz"
+req = requests.get(url)
+df = pd.read_csv(io.BytesIO(gzip.decompress(req.content)))
+
+# suppression valeur fonciere nulle
+df.dropna(subset=['valeur_fonciere'], inplace=True)
+df.dropna(subset=['longitude', 'latitude'], inplace=True)
+
+# Selection des colonnes utiles
+dataset = df[['id_mutation',
+              'date_mutation',
+              'nature_mutation',
+              'adresse_nom_voie',
+              'code_postal',
+              'code_commune',
+              'code_departement',
+              'nombre_lots',
+              'type_local',
+              'surface_reelle_bati',
+              'nombre_pieces_principales',
+              'code_nature_culture',
+              'code_nature_culture_speciale',
+              'surface_terrain',
+              'valeur_fonciere',
+              'longitude',
+              'latitude'
+              ]]
+
+# Passage de la colonnes date mutation en datetime puis en seconde pour avoir des int
+dataset.loc[:, 'date_mutation'] = pd.to_datetime(dataset['date_mutation'], format='mixed')
+dataset.loc[:, 'date_mutation'] = dataset['date_mutation'].apply(lambda x: x.timestamp())
+
+# Remplissage des valeurs NaN nature_mutation et labelisation
+dataset.loc[:, 'nature_mutation'] = dataset.nature_mutation.fillna("")
+
+abrv_voie = pd.read_csv(filepath_or_buffer='Data_Files/ABREVIATION_VOIE.csv', sep=',').values
+
+dataset.loc[:, 'adresse_nom_voie'] = dataset.adresse_nom_voie.fillna("")
+
+codex_voie = list()
+for i in dataset.adresse_nom_voie.values:
+
+    list_nom_rue = i.split(' ')
+    premier_valeur_liste = list_nom_rue[0]
+
+    if premier_valeur_liste in abrv_voie:
+        codex_voie.append(premier_valeur_liste)
+    else:
+        codex_voie.append('AUTRE')
+
+dataset.loc[:, 'prefixe_voie'] = codex_voie
+dataset.loc[:, 'code_postal'] = dataset.code_postal.fillna(0)
+dataset.loc[:, 'surface_reelle_bati'] = dataset.surface_reelle_bati.fillna(0)
+dataset.loc[:, 'surface_terrain'] = dataset.surface_terrain.fillna(0)
+dataset.loc[:, 'type_local'] = dataset.type_local.fillna('Autre')
+dataset.loc[:, 'nombre_pieces_principales'] = dataset.nombre_pieces_principales.fillna(0)
+dataset.loc[:, 'code_nature_culture'] = dataset.code_nature_culture.fillna("")
+dataset.loc[:, 'code_nature_culture_speciale'] = dataset.code_nature_culture_speciale.fillna("")
+
+classe_liste_nature_mutation = list(dict.fromkeys(list(dataset.nature_mutation.values)))
+classe_liste_code_type_local = list(dict.fromkeys(list(dataset.type_local.values)))
+classe_liste_prefixe_voie = list(dict.fromkeys(codex_voie))
+classe_liste_code_culture = pd.read_csv(filepath_or_buffer='Data_Files/CODE_CULTURE.csv', sep=',')
+classe_liste_code_culture = list(classe_liste_code_culture['Code_nature_culture'].to_dict().values())
+classe_liste_code_culture_spe = pd.read_csv(filepath_or_buffer='Data_Files/CODE_CULTURE_SPECIALE.csv', sep=',')
+classe_liste_code_culture_spe = list(classe_liste_code_culture_spe['CODE_CULTURE_SPECIALE'].to_dict().values())
+Nom_colonnes_from_dataset = [
+    'date_mutation',
+    'code_postal',
+    'code_commune',
+    'code_departement',
+    'nombre_lots',
+    'surface_reelle_bati',
+    'nombre_pieces_principales',
+    'surface_terrain',
+    'valeur_fonciere',
+    'longitude',
+    'latitude'
+]
+
+colonnes = Nom_colonnes_from_dataset + classe_liste_code_type_local + classe_liste_prefixe_voie + classe_liste_nature_mutation + classe_liste_code_culture + classe_liste_code_culture_spe
+colonnes = np.array(colonnes)
+del classe_liste_nature_mutation, classe_liste_code_type_local, classe_liste_prefixe_voie, classe_liste_code_culture, classe_liste_code_culture_spe
+
+test = dataset.loc[0:100000]
+
+list_id_mutation = list(dict.fromkeys(test.id_mutation.values))
+
+th_liste = list(range(len(list_id_mutation)))
+Data = pd.DataFrame()
+
+t1 = time.time()
+for i, id in enumerate(list_id_mutation):
+    tmp = test.loc[test.id_mutation == id, :]
+    th_liste[i]: My_thread = My_thread(id, tmp, colonnes)
+    th_liste[i].start()
+    test = test.drop(test[test.id_mutation == id].index)
+
+while len(th_liste) != 0:
+    th_liste[0].join()
+    Data = pd.concat([Data, th_liste[0].retour], axis=0)
+    del th_liste[0]
+
+Data = Data.fillna(0)
+print(t1-time.time())
+
+
+Data.to_csv(path_or_buf='C:/Users/dargo/Files_clean/data_set_clean.csv', sep=',', index=False)
